c658b6199ad2e7284c168f363d342aaa32b25a2d
ashikul akash <51879563+ashikulakash@users.noreply.github.com>
2021-03-16 17:19:49 +0600

    Add files via upload

---
+++ half donation form.py
@@ -0,0 +1,18 @@
+num1=input("Type input: ")
+num2=input("Type input: ")
+num1=int(num1)
+num2=int(num2)
+
+sum=num1+num2
+
+a="My nam is"
+b=input("What is you name? ")
+c="my total="
+
+print(a, b, ",", c, sum, ".")
+
+give=sum/2
+
+begger=input("Fill your name to take half share: ")
+
+print("I will give", begger,"-", give, "pices")
