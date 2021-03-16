8592ecd889b068b56f405498f890d45f9014b897
ashikul akash <51879563+ashikulakash@users.noreply.github.com>
2021-03-16 17:28:05 +0600

    Update and rename half donation form.py to mango donation form.py

---
+++ mango donation form.py
@@ -0,0 +1,19 @@
+nam=input("What is you name? ")
+
+num1=input("mangos from garden: ")
+num2=input("mangos from market: ")
+num1=int(num1)
+num2=int(num2)
+
+sum=num1+num2
+
+a="My nam is"
+b="my total mangos="
+
+print(a, nam, ",", b, sum, ".")
+
+give=sum/2
+
+begger=input("Fill your name to take half share of the mangos: ")
+
+print("I will give", begger,"-", give, "pices mango")
